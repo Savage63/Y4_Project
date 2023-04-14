74e59b47e22fa110ebb011f29fa42f072ad8f9d4
Savage63 <72495447+Savage63@users.noreply.github.com>
2023-04-14 03:41:55 +0100

    Update User.java

--- User.java
+++ User.java
@@ -24,7 +24,7 @@ public class User
     private JTextField textField;
     private JTable table;
 
-    //Launch the application.
+    //Launches the application.
     public static void main(String[] args) 
     {
         EventQueue.invokeLater(new Runnable() 
@@ -43,20 +43,23 @@ public class User
         });
     }
 
-    //Create the application.
+    //Creates the application.
     public User() 
     {
+    	//Creates The GUI Frame
         UserPage = new JFrame();
         UserPage.setTitle("User Page");
         UserPage.setBounds(100, 100, 799, 400);
         UserPage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         UserPage.getContentPane().setLayout(null);
 
+        //Creates a Panel in the JFrame
         JPanel panel = new JPanel();
         panel.setBounds(0, 0, 108, 362);
         UserPage.getContentPane().add(panel);
         panel.setLayout(null);
 
+        //Creates Buttons and Sets Action Listeners to them
         JButton HomeButton = new JButton("Home");
 	    HomeButton.setBounds(10, 11, 89, 23);
 	    panel.add(HomeButton);
@@ -64,8 +67,8 @@ public class User
 	    {
 	        public void actionPerformed(ActionEvent e) 
 	        {
-	        	UserPage.dispose(); // Dispose the current frame
-	        	Home.main(null);
+	        	UserPage.dispose(); //Closes the current frame
+	        	Home.main(null); //Opens Home.java
 	        }
 	    });
 
@@ -76,8 +79,8 @@ public class User
 	    {
 	        public void actionPerformed(ActionEvent e) 
 	        {
-	        	UserPage.dispose(); // Dispose the current frame
-	            Scan.main(null);
+	        	UserPage.dispose(); //Closes the current frame
+	            Scan.main(null); //Opens Scan.java
 	        }
 	    });
 
@@ -88,8 +91,8 @@ public class User
 	    {
 	        public void actionPerformed(ActionEvent e) 
 	        {
-	        	UserPage.dispose(); // Dispose the current frame
-	            Result.main(null);
+	        	UserPage.dispose(); //Closes the current frame
+	            Result.main(null); //Opens Result.java
 	        }
 	    });
 
@@ -100,8 +103,8 @@ public class User
 	    {
 	        public void actionPerformed(ActionEvent e) 
 	        {
-	        	UserPage.dispose(); // Dispose the current frame
-	            User.main(null);
+	        	UserPage.dispose(); //Closes the current frame
+	            User.main(null); //Opens User.java
 	        }
 	    });
 	    
@@ -112,22 +115,24 @@ public class User
 	    {
 	        public void actionPerformed(ActionEvent e) 
 	        {
-	        	UserPage.dispose(); // Dispose the current frame
-	            Contacts.main(null);
+	        	UserPage.dispose(); //Closes the current frame
+	            Contacts.main(null); //Opens Contacts.java
 	        }
 	    });
 	    
-        
+        //Creates a Text Field
         textField = new JTextField();
         textField.setBounds(205, 31, 171, 23);
         UserPage.getContentPane().add(textField);
         textField.setColumns(10);
 
+        //Creates a Drop Box that gives the option 'Permanent' of Temporary'
         JComboBox<Object> comboBox = new JComboBox<Object>();
         comboBox.setModel(new DefaultComboBoxModel<Object>(new String[] { "Permanent", "Temporary" }));
         comboBox.setBounds(402, 31, 125, 23);
         UserPage.getContentPane().add(comboBox);
 
+        //Creates a JTable that stores the variable in the  Text Field and Drop Box
         table = new JTable();
         table.setModel(new DefaultTableModel(
             new Object[][] 
@@ -142,15 +147,18 @@ public class User
         UserPage.getContentPane().add(table);
         table.setEnabled(false);
 
+        
         DefaultTableModel model = (DefaultTableModel) table.getModel();
         
-        // Read data from file and add to table model
+        //Read data from myusers.txt and add to the table
         try (Scanner scanner = new Scanner(new File("myusers.txt"))) 
         {
+        	//Makes a Scanner that scans the file for data and splits the words at a ,
             while (scanner.hasNextLine()) 
             {
                 String line = scanner.nextLine();
                 String[] parts = line.split(",");
+                //Prints the contents of each word into each cell of the row, and prints "Click to Delete User" in the third cell.
                 model.addRow(new Object[] { parts[0], parts[1], "Click to Delete User" });
             }
         } 
@@ -160,7 +168,7 @@ public class User
         }
 
 
-        // Add mouse listener to table
+        // Add mouse listener to the Third cell of each row.
         table.addMouseListener(new MouseAdapter() 
         {
             @Override
@@ -169,19 +177,23 @@ public class User
                 int row = table.rowAtPoint(e.getPoint());
                 int column = table.columnAtPoint(e.getPoint());
 
-                // Check if third cell is clicked
-                if (column == 2) {
-                    // Remove row
+                // Check if third cell is clicked.
+                if (column == 2) 
+                {
+                    //Remove row from Table.
                     model.removeRow(row);
                     
-                    // Update file
+                    //Update "myusers.txt" to remove the user.
                     try (BufferedWriter bw = new BufferedWriter(new FileWriter("myusers.txt")))
                     {
-                        for (int i = 0; i < model.getRowCount(); i++) {
+                        for (int i = 0; i < model.getRowCount(); i++) 
+                        {
                             bw.write(model.getValueAt(i, 0) + "," + model.getValueAt(i, 1));
                             bw.newLine();
                         }
-                    } catch (IOException e1) {
+                    } 
+                    catch (IOException e1) 
+                    {
                         e1.printStackTrace();
                     }
                 }
@@ -189,22 +201,27 @@ public class User
 
         });
 
+        //Adds a button to Add a user into the table.
         JButton btnNewButton = new JButton("Add User");
         btnNewButton.setBounds(573, 31, 89, 23);
         UserPage.getContentPane().add(btnNewButton);
-        btnNewButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
+        btnNewButton.addActionListener(new ActionListener() 
+        {
+            public void actionPerformed(ActionEvent e) 
+            {
                 String name = textField.getText();
                 if (name.isEmpty()) 
                 {
+                	//When the button is pressed it will check if the Text Field has any text in it and if no text is found, it will tell the user to enter a name.
                     JOptionPane.showMessageDialog(UserPage, "Please enter a name");
                 } 
                 else 
                 {
+                	//Takes the Text and the Drop Box and adds them into a file called "myusers.txt"
                     String type = comboBox.getSelectedItem().toString();
                     model.addRow(new Object[] { name, type });
 
-                    // Update file
+                    //Updates "myusers.txt"
                     try (BufferedWriter bw = new BufferedWriter(new FileWriter("myusers.txt", true))) 
                     {
                         bw.write(name + "," + type);
@@ -215,14 +232,13 @@ public class User
                         e1.printStackTrace();
                     }
 
-                    // Clear text field and update table
+                    //Clear text field and update table
                     textField.setText("");
                     table.setModel(model);
-                    UserPage.dispose(); // Dispose the current frame
-    	            User.main(null);
+                    UserPage.dispose(); //Closes the current frame
+    	            User.main(null); //Opens User.java
                 }
             }
         });
-
     }
 }
