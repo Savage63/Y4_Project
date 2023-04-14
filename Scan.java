5a6b1b21427922ae5a2f9b02fcc0194e07257f5f
Savage63 <72495447+Savage63@users.noreply.github.com>
2023-04-14 03:50:01 +0100

    Update Scan.java

--- Scan.java
+++ Scan.java
@@ -3,7 +3,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.net.InetAddress;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JEditorPane;
