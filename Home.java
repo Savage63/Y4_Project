55037a48da925d91b712b45409f5e49c324a0db2
Savage63 <72495447+Savage63@users.noreply.github.com>
2023-04-17 22:33:39 +0100

    Update Home.java

--- Home.java
+++ Home.java
@@ -128,22 +128,30 @@ public class Home
 	{
 	    
 		//Sets the text Displayed in the JTextArea
-	    textArea.setText("\t\t         			Welcome to the Home Page!\r\n\r\n\r\n\r\n   "
+	    textArea.setText("\t\t         			Welcome to the Home Page!\r\n\r\n\r\n\r\n  "
 	    		
 	    				+ "The aim for this Application is to provide our Users with a way to Scan their Network, and keep track of the devices and users that may be connected. "
-	    				+ "The application also displays the information found from the performed scan and allows the you to view each device and its necessary information. \r\n   "
-	    				+ "The hope is to allow the User to have their own Home Network Security \r\n\r\n\r\n   "
+	    				+ "The application also displays the information found from the performed scan and allows the user to view each device and its necessary information. \r\n  "
+	    				+ "The hope is to allow the User to have their own form of Home Network Security \r\n\r\n\r\n  "
 	    				
-	    				+ "When on a different Page, the 'Home' button will bring you back to this page. \r\n\r\n\r\n   "
+						+ "The design and layout of the project is to allow the User to navigate easily and without concern. \r\n  "
+						+ "The only requirement for this project is to install Nmap. It can be installed at https://nmap.org/download.html \r\n\r\n\r\n\r\n  "
+						
+						+ "The functions for the buttons on the side are as follows: \r\n\r\n  "
 	    				
-	    				+ "The 'Scan' button brings you to a seperate page that when the main button is pressed, it will use Nmap (A Network Scanner). "
-	    				+ "This will perform an intense scan on your entire network. This scan performs best when all devices in the household is powered on before commencing the scan.\r\n   "
-	    				+ "Once the scan is finished it will open another page that will show you any devices found in the scan and the information found for each device. "
+	    				+ "When on a different Page, the 'Home' button will bring you back to this page. \r\n\r\n  "
 	    				
-	    				+ "The issue with Nmap is that it may not be very accurate so apologies if there is missing information.\r\n\r\n\r\n   "
+	    				+ "The 'Scan' button brings you to a separate page that when the main 'Scan Network' button is pressed, it will use Nmap (A Network Scanner). "
+	    				+ "This will perform a scan on your entire network by taking your devices local IP. This scan performs best when all devices in the household is powered on before commencing the scan.\r\n  "
+	    				+ "Once the scan is finished it will open another page that will show you the results of any devices found in the scan and the information found for each device. "
 	    				
-	    				+ "The 'Results' button brings you to a seperate page that displays the results performed by the Scan. If there was no scan performed, the Results tab will prompt the user to perform a Scan before viewing results.\r\n\r\n\r\n   The 'Users' Button will allow you to keep track of the users that you have on the network. You will be able to crate a user so that it is saved and recoded. You will then be able to mark each user as Permanent or Temporary,\r\n   depending on whether they are visiting or living in the household. It is advisable to keep this record updated as it can help secure your Network.\r\n\r\n\r\n   "
+	    				+ "The issue with Nmap is that it may not be very accurate so apologies if there is missing information.\r\n\r\n\r\n  "
 	    				
-	    				+ "The 'Contact' Button will show you the contact information for the Developer. If you have any issues or quesries, please contact us.\r\n");
+	    				+ "The 'Results' button brings you to a seperate page that displays the results performed by the Scan. If there was no scan performed, the Results tab will prompt the user to perform a Scan before viewing results.\r\n\r\n\r\n  "
+	    				
+	    				+ "The 'Users' Button will allow you to keep track of the users that you have on the network. You will be able to create a user so that it is saved and recoded. You will then be able to mark each user as Permanent or Temporary,\r\n  "
+	    				+ "depending on whether they are visiting or living in the household. It is advisable to keep this record updated as it can help secure your Network and keep track of its users.\r\n\r\n\r\n  "
+	    				
+	    				+ "The 'Contact' Button will show you the contact information for the Developer. If you have any issues or queries, please get in contact.\r\n");
 	}
 }
