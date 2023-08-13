c722d82fca5ddd021912ff52050f5094e9ab9a7f
Savage63 <72495447+Savage63@users.noreply.github.com>
2023-08-13 16:22:53 +0100

    Update Scan.java

--- Scan.java
+++ Scan.java
@@ -12,7 +12,7 @@ public class Scan
 {
     JFrame ScanPage;
 
-    //Launches the application.
+    // Launches the application.
     public static void main(String[] args) 
     {
         EventQueue.invokeLater(new Runnable() 
@@ -32,17 +32,17 @@ public class Scan
         });
     }
 
-    //Creates the application.
+    // Creates the application.
     public Scan() 
     {
-    	//Creates The GUI Frame
+        // Creates The GUI Frame
         ScanPage = new JFrame();
         ScanPage.setTitle("Scanner Page");
         ScanPage.setBounds(100, 100, 1189, 551);
         ScanPage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ScanPage.getContentPane().setLayout(null);
 
-        //Add a button to the content pane using absolute layout
+        // Add a button to the content pane using absolute layout
         JButton ScanNetworkButton = new JButton("Scan Network");
         ScanNetworkButton.setBounds(406, 149, 400, 166);
         ScanPage.getContentPane().add(ScanNetworkButton);
@@ -55,27 +55,44 @@ public class Scan
                     // Get the local IP address of the user's machine
                     InetAddress localhost = InetAddress.getLocalHost();
                     String localIpAddress = localhost.getHostAddress();
-                    
-                    //Changes the last digits in the local IP address to ".0/24" eg. 192.168.1.2 becomes 192.168.1.0/24
-                    String networkAddress = localIpAddress.substring(0, localIpAddress.lastIndexOf(".")) + ".0/24";
-                    
-                    //Builds the Nmap command with the IP address to scan
-                    ProcessBuilder pb = new ProcessBuilder("nmap", "-sS", "-O", networkAddress);
 
-                    //Creates folder called Project and Redirects Nmap output to a file called "nmap_output.txt" in the home directory
-                    File projectDir = new File(System.getProperty("user.home"), "Project");
+                    // Changes the last digits in the local IP address to ".0/24" eg. 192.168.1.2 becomes 192.168.1.0/24
+                    String networkAddress = localIpAddress.substring(0, localIpAddress.lastIndexOf(".")) + ".0/24";
+
+                    // Build the Nmap command with the IP address to scan
+                    String osName = System.getProperty("os.name").toLowerCase();
+                    ProcessBuilder pb;
+                    if (osName.contains("win")) 
+                    {
+                        pb = new ProcessBuilder("cmd", "/c", "nmap -F -A -T4 " + networkAddress);
+                    } 
+                    else 
+                    {
+                        pb = new ProcessBuilder("bash", "-c", "nmap -F -A -T4 " + networkAddress);
+                    }
+
+                    // Creates folder called "Project" and gets the path to the nmap_output.txt file
+                    File projectDir = new File(System.getProperty("user.home") + File.separator + "Project");
                     projectDir.mkdir(); // create the directory if it does not exist
                     File outputFile = new File(projectDir, "nmap_output.txt");
+                    
+                    // Delete the old nmap_output.txt if it exists
+                    if (outputFile.exists()) 
+                    {
+                        outputFile.delete();
+                    }
+
+                    // Redirect Nmap output to the new nmap_output.txt file
                     pb.redirectOutput(ProcessBuilder.Redirect.to(outputFile));
 
-                    //Starts the Nmap process
+                    // Starts the Nmap process
                     Process process = pb.start();
 
-                    //Waits for the Nmap process to finish
+                    // Waits for the Nmap process to finish
                     int exitCode = process.waitFor();
                     System.out.println("nmap scan finished with exit code " + exitCode);
 
-                    //Opens the Results.java page
+                    // Opens the Results.java page
                     ScanPage.dispose(); // Dispose the current frame
                     Result.main(null);
 
@@ -87,12 +104,12 @@ public class Scan
             }
         });
 
-        //Creates Panel holding the buttons
+        // Creates Panel holding the buttons
         JPanel panel = new JPanel();
         panel.setBounds(0, 0, 108, 512);
         ScanPage.getContentPane().add(panel);
         panel.setLayout(null);
-
+        
         //Creates Buttons and Sets Action Listeners to them
 	    JButton HomeButton = new JButton("Home");
 	    HomeButton.setBounds(10, 27, 89, 23);
@@ -153,7 +170,6 @@ public class Scan
 	            Contacts.main(null); //Opens Contacts.java
 	        }
 	    });
-	    
 
         JEditorPane editorPane = new JEditorPane();
         editorPane.setEditable(false);
