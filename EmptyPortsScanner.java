868010bc44659216666f4f2014b4dfb1ea5587da
Savage63 <72495447+Savage63@users.noreply.github.com>
2023-08-13 16:21:17 +0100

    Update EmptyPortsScanner.java

--- EmptyPortsScanner.java
+++ EmptyPortsScanner.java
@@ -1,48 +1,72 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class EmptyPortsScanner {
-    public static void main(String[] args) {
-        String targetIpAddress = "192.168.0.22"; // Replace with the target device's IP address
-        String nmapCommand = "nmap -p- " + targetIpAddress; // -p- option scans all 65535 ports
+public class EmptyPortsScanner 
+{
+    private static String outputFolderPath = System.getProperty("user.home") + File.separator + "Project" + File.separator + "Devices";
+
+    public static boolean scan(String targetIpAddress, String outputFilePath) throws IOException 
+    {
+        String nmapCommand = "nmap -p- " + targetIpAddress;
         boolean emptyPortsFound = false;
 
-        try {
+        try 
+        {
             ProcessBuilder processBuilder;
             Process process;
 
-            if (System.getProperty("os.name").startsWith("Windows")) {
-                // Use "cmd" and "/c" for Windows
-                processBuilder = new ProcessBuilder("cmd", "/c", nmapCommand);
-            } else {
-                // Use "bash" and "-c" for other operating systems
-                processBuilder = new ProcessBuilder("bash", "-c", nmapCommand);
+            File scanFolder = new File(outputFolderPath, targetIpAddress);
+            if (!scanFolder.exists()) 
+            {
+                scanFolder.mkdirs();
+            }
+
+            if (System.getProperty("os.name").startsWith("Windows")) 
+            {
+                processBuilder = new ProcessBuilder("cmd", "/c", nmapCommand + " > " + outputFilePath);
+            } 
+            else 
+            {
+                processBuilder = new ProcessBuilder("bash", "-c", nmapCommand + " > " + outputFilePath);
             }
 
             processBuilder.redirectErrorStream(true);
             process = processBuilder.start();
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                // Parse the output of Nmap to find open and empty ports
-                if (line.contains("/tcp") && line.contains("closed")) {
-                    // Here, you can process the empty port information as needed
-                    System.out.println("Empty port found: " + line);
-                    emptyPortsFound = true;
-                }
-            }
-
             process.waitFor();
-            reader.close();
-        } catch (IOException | InterruptedException e) {
+
+        } 
+        catch (IOException | InterruptedException e) 
+        {
             e.printStackTrace();
         }
 
-        if (!emptyPortsFound) {
-            System.out.println("No Empty Ports Found");
+        return emptyPortsFound;
+    }
+
+    public static String getEmptyPorts(String scanFilePath) throws IOException 
+    {
+        List<String> emptyPortsList = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(scanFilePath)))) 
+        {
+            String line;
+            while ((line = reader.readLine()) != null) 
+            {
+                if (line.contains("/tcp") && line.contains("closed")) 
+                {
+                    int startIndex = line.indexOf("/") + 1;
+                    int endIndex = line.indexOf("/tcp");
+                    String port = line.substring(startIndex, endIndex);
+                    emptyPortsList.add(port);
+                }
+            }
+        } 
+        catch (IOException e) 
+        {
+            e.printStackTrace();
         }
+
+        return String.join(", ", emptyPortsList);
     }
 }
