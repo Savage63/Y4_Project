5bb532aa3609f8fade1507f0eaf6f739d153ebe9
Savage63 <72495447+Savage63@users.noreply.github.com>
2023-08-23 15:14:54 +0100

    Update EmptyPortCloser.java

--- EmptyPortCloser.java
+++ EmptyPortCloser.java
@@ -1,34 +1,48 @@
 import java.net.ServerSocket;
+import java.util.List;
 
 public class EmptyPortCloser 
 {
-
-    public void closeSpecifiedPorts() 
+    public void closeSpecifiedPorts(List<String> emptyPorts) 
     {
-        int[] portsToClose = {80, 443, 8080}; //Ports to close
-
-        for (int port : portsToClose) 
+        for (String portString : emptyPorts) 
         {
             try 
             {
+                int port = Integer.parseInt(portString);
+
                 // Create a ServerSocket to bind to the specified port
-                ServerSocket serverSocket = new ServerSocket(port);
-
-                // Close the ServerSocket to "close" the port
-                serverSocket.close();
-
-                System.out.println("Port " + port + " is now closed\n");
+                try (ServerSocket serverSocket = new ServerSocket(port)) 
+                {
+                    // Close the ServerSocket to "close" the port
+                    System.out.println("Port " + port + " is now closed");
+                } 
+                catch (Exception e) 
+                {
+                    System.out.println("Failed to close port " + port + ": " + e.getMessage());
+                }
             } 
-            catch (Exception e)
+            catch (NumberFormatException e) 
             {
-                System.out.println("Failed to close port " + port + ": " + e.getMessage() + "\n");
+                System.out.println("Invalid port number: " + portString);
             }
         }
     }
 
+    public static List<String> getEmptyPortsFromScannerOrPage() 
+    {
+        // Retrieve the list of empty ports from RowInformationPage
+        RowInformationPage rowInfoPage = new RowInformationPage(new Object[]{"Device 1", "0.0.0.0", "00:11:22:33:44:55", "Linux", "Manufacturer"});
+        // Assuming there's a method in RowInformationPage to get the empty ports
+        List<String> emptyPorts = rowInfoPage.getEmptyPorts();
+
+        return emptyPorts;
+    }
+
     public static void main(String[] args)
     {
+        List<String> emptyPorts = getEmptyPortsFromScannerOrPage();
         EmptyPortCloser closePorts = new EmptyPortCloser();
-        closePorts.closeSpecifiedPorts();
+        closePorts.closeSpecifiedPorts(emptyPorts);
     }
 }
