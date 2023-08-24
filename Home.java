9f6ab1a2b707c25b381fd48a0f7b9ce72cd3eba7
Savage63 <72495447+Savage63@users.noreply.github.com>
2023-08-24 02:30:49 +0100

    Update Home.java

--- Home.java
+++ Home.java
@@ -5,75 +5,97 @@ import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
+import javax.swing.JEditorPane;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
 
 public class Home 
 {
-	private JFrame HomePage;
+    private JFrame Contact;
 
+    // Launches the application.
+    public static void main(String[] args) 
+    {
+        EventQueue.invokeLater(new Runnable() 
+        {
+            public void run() 
+            {
+                try 
+                {
+                    Home window = new Home();
+                    window.Contact.setVisible(true);
+                } 
+                catch (Exception e) 
+                {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-	//Launches the application.
-	public static void main(String[] args) 
-	{
-		EventQueue.invokeLater(new Runnable() 
-		{
-			public void run() 
-			{
-				try 
-				{
-					Home window = new Home();
-					window.HomePage.setVisible(true);
-				} 
-				catch (Exception e) 
-				{
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    // Creates the application.
+    public Home() 
+    {
+        // Creates The GUI Frame
+        Contact = new JFrame();
+        Contact.setTitle("Home Page");
+        Contact.setBounds(100, 100, 1189, 551);
+        Contact.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Contact.getContentPane().setLayout(null);
+	    Contact.setResizable(false); // Make the frame not resizable
 
- 	//Creates the application.
-	public Home() 
-	{ 
-		//Create The GUI Frame
-	    HomePage = new JFrame();
-	    HomePage.setTitle("Home Page");
-	    HomePage.setBounds(100, 100, 1189, 551);
-	    HomePage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    HomePage.getContentPane().setLayout(null);
-	    HomePage.setResizable(false); // Make the frame not resizable
+        // Adds a JScrollPane to the GUI Frame
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setBounds(107, 11, 1066, 501);
+        Contact.getContentPane().add(scrollPane);
 
-	    //Adds a JScrollPane to the GUI Frame
-	    JScrollPane scrollPane = new JScrollPane();
-	    scrollPane.setBounds(107, 11, 1066, 501);
-	    HomePage.getContentPane().add(scrollPane);
+        // Creates a new JEditorPane and adds it to the JScrollPane
+        JEditorPane editorPane = new JEditorPane();
+        editorPane.setEditable(false);
+        editorPane.setContentType("text/html"); // Set content type to HTML
+        scrollPane.setViewportView(editorPane);
 
-	    //Creates a new JTextArea and adds it to the JScrollPane
-	    JTextArea HomeText = new JTextArea();
-	    HomeText.setEditable(false);
-	    scrollPane.setViewportView(HomeText);
+        // Add a HyperlinkListener to open links in a web browser
+        editorPane.addHyperlinkListener(new HyperlinkListener() 
+        {
+            @Override
+            public void hyperlinkUpdate(HyperlinkEvent e) 
+            {
+                if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) 
+                {
+                    try 
+                    {
+                        java.awt.Desktop.getDesktop().browse(e.getURL().toURI());
+                    } 
+                    catch (Exception ex) 
+                    {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        });
 
-	    //Calls the displayText() method to set the text in the JTextArea
-	    displayText(HomeText);
+        // Calls the displayText() method to set the HTML content in the JEditorPane
+        displayText(editorPane);
 
-	    //Creates a Panel in the JFrame
-	    JPanel panel = new JPanel();
-	    panel.setBounds(0, 0, 108, 512);
-	    HomePage.getContentPane().add(panel);
-	    panel.setLayout(null);
+        // Creates a Panel in the JFrame
+        JPanel panel = new JPanel();
+        panel.setBounds(0, 0, 108, 512);
+        Contact.getContentPane().add(panel);
+        panel.setLayout(null);
 
-	    //Creates Buttons and Sets Action Listeners to them
-	    JButton HomeButton = new JButton("Home");
-	    HomeButton.setBounds(10, 27, 89, 23);
-	    panel.add(HomeButton);
-	    HomeButton.addActionListener(new ActionListener() 
-	    {
-	        public void actionPerformed(ActionEvent e) 
-	        {
-	        	HomePage.dispose(); //Closes the current frame
-	        	Home.main(null); //Opens Home.java
-	        }
-	    });
+        // Creates Buttons and Sets Action Listeners to them
+        JButton HomeButton = new JButton("Home");
+        HomeButton.setBounds(10, 27, 89, 23);
+        panel.add(HomeButton);
+        HomeButton.addActionListener(new ActionListener() 
+        {
+            public void actionPerformed(ActionEvent e) 
+            {
+                Contact.dispose(); // Closes the current frame
+                Home.main(null); // Opens Home.java
+            }
+        });
 
 	    JButton ScanButton = new JButton("Scan");
 	    ScanButton.setBounds(10, 61, 89, 23);
@@ -82,7 +104,7 @@ public class Home
 	    {
 	        public void actionPerformed(ActionEvent e) 
 	        {
-	        	HomePage.dispose(); //Closes the current frame
+	        	Contact.dispose(); //Closes the current frame
 	            Scan.main(null); //Opens Scan.java
 	        }
 	    });
@@ -94,7 +116,7 @@ public class Home
 	    {
 	        public void actionPerformed(ActionEvent e) 
 	        {
-	        	HomePage.dispose(); //Closes the current frame
+	        	Contact.dispose(); //Closes the current frame
 	            Result.main(null); //Opens Result.java
 	        }
 	    });
@@ -106,52 +128,51 @@ public class Home
 	    {
 	        public void actionPerformed(ActionEvent e) 
 	        {
-	        	HomePage.dispose(); //Closes the current frame
+	        	Contact.dispose(); //Closes the current frame
 	            User.main(null); //Opens User.java
 	        }
 	    });
 	    
 	    JButton ContactButton = new JButton("Contact");
-	    ContactButton.setBounds(10, 423, 89, 23);
-	    panel.add(ContactButton);
-	    ContactButton.addActionListener(new ActionListener() 
-	    {
-	        public void actionPerformed(ActionEvent e) 
-	        {
-	        	HomePage.dispose(); //Closes the current frame
-	            Contacts.main(null); //Opens Contacts.java
-	        }
-	    });
-	}
-	
-	private void displayText(JTextArea textArea)
-	{
-	    
-		//Sets the text Displayed in the JTextArea
-	    textArea.setText("\t\t         			Welcome to the Home Page!\r\n\r\n\r\n\r\n  "
-	    		
-	    				+ "The aim for this Application is to provide our Users with a way to Scan their Network, and keep track of the devices and users that may be connected. "
-	    				+ "The application also displays the information found from the performed scan and allows the user to view each device and its necessary information. \r\n  "
-	    				+ "The hope is to allow the User to have their own form of Home Network Security \r\n\r\n\r\n  "
-	    				
-						+ "The design and layout of the project is to allow the User to navigate easily and without concern. \r\n  "
-						+ "The only requirement for this project is to install Nmap. It can be installed at https://nmap.org/download.html \r\n\r\n\r\n\r\n  "
-						
-						+ "The functions for the buttons on the side are as follows: \r\n\r\n  "
-	    				
-	    				+ "When on a different Page, the 'Home' button will bring you back to this page. \r\n\r\n  "
-	    				
-	    				+ "The 'Scan' button brings you to a separate page that when the main 'Scan Network' button is pressed, it will use Nmap (A Network Scanner). "
-	    				+ "This will perform a scan on your entire network by taking your devices local IP. This scan performs best when all devices in the household is powered on before commencing the scan.\r\n  "
-	    				+ "Once the scan is finished it will open another page that will show you the results of any devices found in the scan and the information found for each device. "
-	    				
-	    				+ "The issue with Nmap is that it may not be very accurate so apologies if there is missing information.\r\n\r\n\r\n  "
-	    				
-	    				+ "The 'Results' button brings you to a seperate page that displays the results performed by the Scan. If there was no scan performed, the Results tab will prompt the user to perform a Scan before viewing results.\r\n\r\n\r\n  "
-	    				
-	    				+ "The 'Users' Button will allow you to keep track of the users that you have on the network. You will be able to create a user so that it is saved and recoded. You will then be able to mark each user as Permanent or Temporary,\r\n  "
-	    				+ "depending on whether they are visiting or living in the household. It is advisable to keep this record updated as it can help secure your Network and keep track of its users.\r\n\r\n\r\n  "
-	    				
-	    				+ "The 'Contact' Button will show you the contact information for the Developer. If you have any issues or queries, please get in contact.\r\n");
-	}
+        ContactButton.setBounds(10, 423, 89, 23);
+        panel.add(ContactButton);
+        ContactButton.addActionListener(new ActionListener() 
+        {
+            public void actionPerformed(ActionEvent e) 
+            {
+                Contact.dispose(); // Closes the current frame
+                Contacts.main(null); // Opens Contacts.java
+            }
+        });
+    }
+
+    private void displayText(JEditorPane editorPane) 
+    {
+        // Sets the HTML content with mailto link
+        editorPane.setText("<html>"
+                + "<h1>Welcome to the Home Page!</h1><br>"
+                + "<h2>About the Application:</h2>"
+                + "<p>The aim for this Application is to provide our Users with a way to Scan their Network, and keep track of the devices and users that may be connected. "
+                + "The application also displays the information found from the performed scan and allows the user to view each device and its necessary information.</p>"
+                + "<p>The hope is to allow the User to have their own form of Home Network Security.</p><br>"
+                
+                + "<h2>Design and Layout:</h2>"
+                + "<p>The design and layout of the project is to allow the User to navigate easily and without concern. "
+                + "The only requirement for this project is to install Nmap. It can be installed at <a href=\"https://nmap.org/download.html\">https://nmap.org/download.html</a>.</p><br>"
+                + "<p>The issue with Nmap is that it may not be very accurate, so apologies if there is missing information.</p><br>"
+                
+                + "<h2>Button Functions:</h2>"
+                + "<p><strong>Home Button:</strong> When on a different Page, the 'Home' button will bring you back to this page.</p>"
+                + "<p><strong>Scan Button:</strong> Brings you to a separate page where pressing the main 'Scan Network' button will use Nmap (A Network Scanner). "
+                + "This will perform a scan on your entire network by taking your devices local IP. This scan performs best when all devices in the household are powered on before commencing the scan."
+                + "Once the scan is finished, it will open another page that will show you the results of any devices found in the scan and the information found for each device.</p><br>"
+                
+                + "<p><strong>Results Button:</strong> Brings you to a separate page that displays the results performed by the Scan. If no scan was performed, the Results tab will prompt the user to perform a Scan before viewing results.</p><br>"
+                
+                + "<p><strong>User Button:</strong> Allows you to keep track of the users on the network. You can create a user so that it is saved and recorded. You can mark each user as Permanent or Temporary, "
+                + "depending on whether they are visiting or living in the household. It is advisable to keep this record updated to help secure your Network and keep track of its users. You can also assign each user a device found in the scan to keep track of each users devices.</p><br>"
+                
+                + "<p><strong>Contact Button:</strong> Shows you the contact information for the Developer. If you have any issues or queries, please get in contact.</p><br>"
+                + "</html>");
+    }
 }
